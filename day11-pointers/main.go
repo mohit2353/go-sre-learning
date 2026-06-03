23321bc36e4d90dc4d7f06eed90e061a29f90ff5
Mohit Mathur <YOUR_EMAIL@gmail.com>
2026-06-03 18:04:37 +0530

    Day 11 Pointer completed

---
+++ day11-pointers/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func changeName(name string) {
+
+	name = "GitLab"
+}
+
+func changeAge(age int) {
+	age = 30
+}
+
+func increaseAge(age int) {
+	age++
+}
+
+func main() {
+
+	name := "Mohit"
+	age := 25
+
+	changeAge(age)
+	increaseAge(age)
+
+	changeName(name)
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println("Age after increase:", age)
+}
